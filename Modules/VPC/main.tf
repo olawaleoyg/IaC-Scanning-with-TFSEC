709ff956e4ff97343db0b14e41d96d21c0ddc466
adeyoyg <olawaleoyg@gmail.com>
2024-08-16 16:08:05 +0100

    Develop Terraform Module for AWS VPC

--- Modules/VPC/main.tf
+++ Modules/VPC/main.tf
@@ -1,1 +1,130 @@
+# Create VPC
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support = true
+  enable_dns_hostnames = true
 
+  tags = merge(
+    var.tags,
+    { Name = "${var.project_name}-vpc" }
+  )
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = merge(
+    var.tags,
+    { Name = "${var.project_name}-igw" }
+  )
+}
+
+# Public Subnets
+resource "aws_subnet" "public" {
+  count = var.public_subnet_count
+
+  vpc_id     = aws_vpc.this.id
+  cidr_block = var.public_subnet_cidrs[count.index]
+  availability_zone = element(var.availability_zones, count.index)
+
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    var.tags,
+    { Name = "${var.project_name}-public-subnet-${count.index + 1}" }
+  )
+}
+
+# Private Subnets
+resource "aws_subnet" "private" {
+  count = var.private_subnet_count
+
+  vpc_id     = aws_vpc.this.id
+  cidr_block = var.private_subnet_cidrs[count.index]
+  availability_zone = element(var.availability_zones, count.index)
+
+  tags = merge(
+    var.tags,
+    { Name = "${var.project_name}-private-subnet-${count.index + 1}" }
+  )
+}
+
+# NAT Gateway
+resource "aws_eip" "this" {
+  count = 1
+  vpc = true
+}
+
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.this[0].id
+  subnet_id     = element(aws_subnet.public[*].id, 0)
+
+  tags = merge(
+    var.tags,
+    { Name = "${var.project_name}-nat-gw" }
+  )
+}
+
+# Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = merge(
+    var.tags,
+    { Name = "${var.project_name}-public-rt" }
+  )
+}
+
+# Associate Public Subnets with Public Route Table
+resource "aws_route_table_association" "public" {
+  count = var.public_subnet_count
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+# Private Route Table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this.id
+  }
+
+  tags = merge(
+    var.tags,
+    { Name = "${var.project_name}-private-rt" }
+  )
+}
+
+# Associate Private Subnets with Private Route Table
+resource "aws_route_table_association" "private" {
+  count = var.private_subnet_count
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+# Default Security Group
+resource "aws_security_group" "default" {
+  vpc_id = aws_vpc.this.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(
+    var.tags,
+    { Name = "${var.project_name}-default-sg" }
+  )
+}
